81b2f6fec9c1d38a67528d43dbdc75d7560511da
suxiaokun <63143425+suxiaokun@users.noreply.github.com>
2020-04-04 18:22:04 +0800

    Update User.java

--- bysj/src/main/java/com/bysj/xqyj/suxk/pojo/User.java
+++ bysj/src/main/java/com/bysj/xqyj/suxk/pojo/User.java
@@ -3,6 +3,7 @@ package com.bysj.xqyj.suxk.pojo;
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import lombok.Data;
 //user 用data包自动生成get/set方法
+//再次新增备注测试
 @Data
 public class User {
     @Excel(name = "账号" ,width = 20)
