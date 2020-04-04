32c6239557730b4fb147c4ca5f4b4ac2519c245f
suxk <1224692968@qq.com>
2020-04-04 18:19:43 +0800

    增加备注修改

--- bysj/src/main/java/com/bysj/xqyj/suxk/pojo/User.java
+++ bysj/src/main/java/com/bysj/xqyj/suxk/pojo/User.java
@@ -2,7 +2,7 @@ package com.bysj.xqyj.suxk.pojo;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import lombok.Data;
-
+//user 用data包自动生成get/set方法
 @Data
 public class User {
     @Excel(name = "账号" ,width = 20)
